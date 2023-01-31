b6126c72dc45e68db9decc99194436542d678493
Bella <Sahnazaroveduard@gmail.com>
2023-01-30 21:46:40 -0500

    I added a new class.

---
+++ src/day1GitHub/ThirdGitHubClass.java
@@ -0,0 +1,9 @@
+package day1GitHub;
+
+public class ThirdGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("i can change whatever i wont in my brunch");
+        System.out.println("*-".repeat(35));
+
+    }
+}
