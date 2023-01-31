fcddf378dd23161e5f75e461c0251f855d822db3
Bella <Sahnazaroveduard@gmail.com>
2023-01-30 20:25:55 -0500

    I added some changes for math

---
+++ src/day1GitHub/FirstGitHubClass.java
@@ -0,0 +1,9 @@
+package day1GitHub;
+
+public class FirstGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hi Git Hub");
+        System.out.println(1+1);
+
+    }
+}
