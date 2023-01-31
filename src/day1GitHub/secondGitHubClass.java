8bdbce111943716b6ca52348b768bdef8d5e09ea
Bella <Sahnazaroveduard@gmail.com>
2023-01-30 20:28:20 -0500

    my second commit

---
+++ src/day1GitHub/secondGitHubClass.java
@@ -0,0 +1,11 @@
+package day1GitHub;
+
+public class secondGitHubClass {
+    public static void main(String[] args) {
+        System.out.println("Hey Git!!!");
+        System.out.println("-".repeat(75));
+        System.out.println("I will try to understand you!!!");
+        System.out.println("-".repeat(75));
+
+    }
+}
