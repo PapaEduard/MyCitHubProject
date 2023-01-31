076d4496b6c114504bb6aca3c899c2c64dcc024c
PapaEduard <123764073+PapaEduard@users.noreply.github.com>
2023-01-30 21:08:18 -0500

    Update FirstGitHubClass.java
    Hey chenged to Hello

--- src/day1GitHub/FirstGitHubClass.java
+++ src/day1GitHub/FirstGitHubClass.java
@@ -2,7 +2,7 @@ package day1GitHub;
 
 public class FirstGitHubClass {
     public static void main(String[] args) {
-        System.out.println("Hi Git Hub");
+        System.out.println("Hello Git Hub");
         System.out.println(1+1);
 
     }
